c4546b683c02c2e9b50bccf772eb746b9a4d257c
Walter Bender <walter@sugarlabs.org>
2010-08-12 14:13:43 -0400

    added glue for building package

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
-from sugar.activity import bundlebuilder
-if __name__ == "__main__":
-    bundlebuilder.start()
+import os
 
+if 'SUGAR_ACTIVITY_ROOT' in os.environ:
+    from sugar.activity import bundlebuilder
+
+    if __name__ == "__main__":
+        bundlebuilder.start()
+else:
+    import glob, os.path, string
+    from distutils.core import setup
+
+    DATA_FILES = [
+        ('icons', glob.glob('icons/*')),
+        ('images', glob.glob('images/*')),
+        ('/usr/share/applications', ['turtleart.desktop'])
+        ]
+
+    setup (name = 'Turtle Art',
+           description = "A LOGO-like tool for teaching programming",
+           author = "Walter Bender",
+           author_email = "walter.bender@gmail.com",
+           version = '0.9.4',
+           packages = ['TurtleArt'],
+           scripts = ['turtleart'],
+           data_files = DATA_FILES,
+           )
