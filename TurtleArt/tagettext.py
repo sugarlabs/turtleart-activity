5bba62b88156e73bca1fa81c7c0aa0c47c0f73e3
Walter Bender <walter.bender@gmail.com>
2011-03-31 01:42:23 -0400

    adding new file for managing global gettext defintion

---
+++ TurtleArt/tagettext.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010-11 Walter Bender, Martin Langhoff
+# License: GPLv2
+
+# Defines the magic global _() with the right params so all modules
+# can use it.
+#
+# Plugins that want to override MUST use a different technique. See
+# the developer notes in the TA wikipage.
+#
+import gettext
+import os
+
+# In a git checkout, locale is in the root of the project
+# so one dir "up" from tagettext.py
+localedir = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                          'locale' )
+
+if os.path.exists(localedir):
+    # works from a git checkout
+    gettext.install('org.laptop.TurtleArtActivity', localedir)
+else:
+    # fallback for packaged TA (rpm, xo)
+    gettext.install('org.laptop.TurtleArtActivity')
