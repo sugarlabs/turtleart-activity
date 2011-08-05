b17732ab595dfb9ce295f091c577e264ff5f9b63
Walter Bender <walter.bender@gmail.com>
2011-08-05 09:45:50 -0400

    adding extra strings for plugins, clones, etc.

---
+++ taextras.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#Copyright (c) 2011 Walter Bender
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+from gettext import gettext as _
+
+'''
+Note: this file contains string from plugins and related projects that
+are not included in the default packaaging of Turtle Art. The reason
+for gathering these strings here is to enable us to use one common POT
+file across all of these projects.
+
+TODO: build some automated way of maintaining this file.
+'''
+
+# ACTIVITIES
+
+TURTLEARTMINI_STRINGS = [
+    _('Turtle Art Mini')
+    ]
+
+TURTLECONFUSION_STRINGS = [
+    _('Turtle Confusion'),
+    _('Select a challenge')
+    ]
+
+# PLUGINS
+
+CURRENCY_STRINGS = [
+    _('Palette of Mexican pesos'),
+    _('Palette of Colombian pesos'),
+    _('Palette of Rwandan francs'),
+    _('Palette of US currencies'),
+    _('Palette of Australian currencies'),
+    _('Palette of Guaranies')
+    ]
+
