c728d70bb4e12b8a48113219b2547bebb543481e
Daniel Drake <drake@endlessm.com>
2015-11-30 09:46:31 -0600

    Fix installation as app
    Update installation scripts for rename to TurtleBlocks, and
    install missing parts.
    
    A TurtleArt.installinfo module is automatically created so that
    TurtleBlocks knows where to find the lib and share resources, which
    is useful for when the app is not installed in /usr.

--- setup.py
+++ setup.py
@@ -7,13 +7,44 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
     # for distutils
     sys.argv.pop(1)
 
+    import os
     import glob
+    import shutil
     from distutils.core import setup
+    from distutils.command.install import install
+
+    class post_install(install):
+        def run(self):
+            install.run(self)
+            print "Running post_install"
+
+            # Create a simple module that allows the app where to discover
+            # its lib and share content.
+            dst = os.path.join(self.install_purelib, "TurtleArt")
+            fd = open(os.path.join(dst, "installinfo.py"), "w")
+            fd.write("INSTALL_PREFIX='%s'\n" % self.prefix)
+            fd.close()
+
+            # distutils doesn't offer a nice way to do recursive install of
+            # a directory tree, so we install the remaining parts here.
+            libdir = os.path.join(self.root + self.install_base, "lib",
+                                  "TurtleBlocks")
+            if not os.path.isdir(libdir):
+                os.makedirs(libdir)
+
+            sharedir = os.path.join(self.root + self.install_base, "share",
+                                    "TurtleBlocks")
+            if not os.path.isdir(sharedir):
+                os.makedirs(sharedir)
+
+            shutil.copytree("plugins", os.path.join(libdir, "plugins"))
+            shutil.copytree("samples", os.path.join(sharedir, "samples"))
 
     DATA_FILES = [
+        ('activity', glob.glob('activity/*')),
         ('icons', glob.glob('icons/*')),
         ('images', glob.glob('images/*')),
-        ('/usr/share/applications', ['turtleart.desktop'])
+        ('/usr/share/applications', ['turtleblocks.desktop'])
     ]
 
     setup(name='Turtle Art',
@@ -22,8 +53,9 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
           author_email="walter.bender@gmail.com",
           version='0.9.4',
           packages=['TurtleArt', 'TurtleArt.util'],
-          scripts=['turtleart'],
+          scripts=['turtleblocks'],
           data_files=DATA_FILES,
+          cmdclass={"install": post_install}
           )
 else:
     from sugar.activity import bundlebuilder
