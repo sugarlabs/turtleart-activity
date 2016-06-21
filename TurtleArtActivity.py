sugarlabs/turtleart-activity

commit 70e9d986cce071a832e76d6f51cc652f2906fc7d

remove glob dependency

TurtleArtActivity.py
@@ -45,7 +45,6 @@ from sugar.datastore import datastore
 from sugar import profile
 
 import os
-import glob
 import tarfile
 import subprocess
 import ConfigParser
@@ -1743,13 +1742,12 @@ class TurtleArtActivity(activity.Activity):
             store.append([pixbuf, filepath])
 
     def _scan_for_samples(self):
-        samples = sorted(
-            glob.glob(
-                os.path.join(
-                    activity.get_bundle_path(),
-                    'samples',
-                    'thumbnails',
-                    '*.png')))
+        path = os.path.join(self._share_path, "samples/thumbnails")
+        samples = []
+        for name in os.listdir(path):
+            if name.endswith(".png"):
+                samples.append(os.path.join(self._share_path, name))
+        samples.sort()
         return samples
 
     def is_toolbar_expanded(self):

TurtleArt/tautils.py
@@ -293,6 +293,14 @@ def json_dump(data):
         jdump(data, io)
         return io.getvalue()
 
+def get_endswith_files(path, end):
+    f = os.listdir(path)
+    files = []
+    for name in f:
+        if name.endswith(end):
+            files.append(os.path.join(path, f))
+    return files
+
 
 def get_load_name(filefilter, load_save_folder=None):
     ''' Open a load file dialog. '''

TurtleArt/turtleblocks.py
@@ -30,7 +30,6 @@ import getopt
 import sys
 import os
 import os.path
-import glob
 import cStringIO
 import errno
 import ConfigParser
@@ -1070,13 +1069,12 @@ Would you like to save before quitting?'))
             store.append([pixbuf, filepath])
 
     def _scan_for_samples(self):
-        samples = sorted(
-            glob.glob(
-                os.path.join(
-                    self._share_path,
-                    'samples',
-                    'thumbnails',
-                    '*.png')))
+        path = os.path.join(self._share_path, "samples/thumbnails")
+        samples = []
+        for name in os.listdir(path):
+            if name.endswith(".png"):
+                samples.append(os.path.join(self._share_path, name))
+        samples.sort()
         return samples
 
 

plugins/turtle_blocks_extras/turtle_blocks_extras.py
@@ -17,7 +17,6 @@
 
 from time import time
 import os
-import glob
 
 from gettext import gettext as _
 
@@ -29,7 +28,8 @@ from TurtleArt.taconstants import (CONSTANTS, MACROS, KEY_DICT, MEDIA_SHAPES,
                                    BLOCKS_WITH_SKIN, PYTHON_SKIN,
                                    MEDIA_BLOCK2TYPE, VOICES)
 from TurtleArt.tautils import (debug_output, get_path, data_to_string,
-                               hat_on_top, listify, data_from_file)
+                               hat_on_top, listify, data_from_file,
+                               get_endswith_files)
 from TurtleArt.taprimitive import (ArgSlot, ConstantArg, Primitive)
 from TurtleArt.tatype import (TYPE_BOOL, TYPE_BOX, TYPE_CHAR, TYPE_INT,
                               TYPE_FLOAT, TYPE_OBJECT, TYPE_STRING,
@@ -1166,7 +1166,7 @@ Journal objects'))
 
         if hasattr(self.tw, 'macros_path') and \
                 os.path.exists(self.tw.macros_path):
-            files = glob.glob(os.path.join(self.tw.macros_path, '*.tb'))
+            files = get_endswith_files(self.tw.macros_path ".tb");
             if len(files) > 0:
                 palette = make_palette(
                     'myblocks',

pysamples/journal-stats.py
@@ -19,7 +19,6 @@ def myblock(tw, x):  # ignore second argument
     ''' Load journal stats to heap (Sugar only) '''
 
     import os
-    import glob
     from gettext import gettext as _
 
     MAX = 19
@@ -35,14 +34,24 @@ def myblock(tw, x):  # ignore second argument
             self._activity_count = []
 
             homepath = os.environ['HOME']
-            for path in glob.glob(os.path.join(homepath, '.sugar', '*')):
+            for name in os.listdir(os.path.join(homepath, ".sugar")):
+                path = os.path.join(homepath, ".sugar", name)
                 if isdsdir(path):
                     self._dsdict[os.path.basename(path)] = []
-                    dsobjdirs = glob.glob(
-                        os.path.join(path, DIROFINTEREST, '??'))
-                    for dsobjdir in dsobjdirs:
-                        dsobjs = glob.glob(os.path.join(dsobjdir, '*'))
-                        for dsobj in dsobjs:
+                    if not os.path.isdir(path):
+                        continue
+
+                    for dsobjdir in os.listdir(path):
+                        if len(dsobjdir) != DIROFINTEREST + 2:
+                            continue
+
+                        dsobjdir = os.path.join(path, dsobjdir)
+                        if not os.path.isdir(dsobjdir):
+                            continue
+
+                        for dsobj in os.listdir(dsobjdir):
+                            dsobj = os.path.join(dsobjdir, dsobj)
+
                             self._dsdict[os.path.basename(path)].append({})
                             activity = isactivity(dsobj)
                             if not activity:

pysamples/ta-stats.py
@@ -19,7 +19,6 @@ def myblock(tw, x):  # ignore second argument
     ''' Load journal stats to heap (Sugar only) '''
 
     import os
-    import glob
 
     _DIROFINTEREST = 'datastore'
 
@@ -31,13 +30,16 @@ def myblock(tw, x):  # ignore second argument
             self._score = []
 
             homepath = os.environ['HOME']
-            for path in glob.glob(os.path.join(homepath, '.sugar', '*')):
+            for name in os.listdir(os.path.join(homepath, '.sugar')):
+                path = os.path.join(homepath, ".sugar", name)
                 if isdsdir(path):
-                    dsobjdirs = glob.glob(
-                        os.path.join(path, _DIROFINTEREST, '??'))
-                    for dsobjdir in dsobjdirs:
-                        dsobjs = glob.glob(os.path.join(dsobjdir, '*'))
-                        for dsobj in dsobjs:
+                    for dsobjdir in os.listdir(path):
+                        if len(dsobjdir) != DIROFINTEREST + 2:
+                            continue
+
+                        dsobjdir = os.path.join(path, dsobjdir)
+                        for dsobj in dsobjdir:
+                            dsobj = os.path.join(dsobjdir, dsobj)
                             if not isactivity(dsobj) == 'TurtleArtActivity':
                                 continue
                             if hascomponent(dsobj, 'mime_type') != \

setup.py
@@ -2,13 +2,18 @@
 
 import sys
 
+def get_files(path):
+    files = []
+    for name in path:
+        files.append(os.path.join(path, name))
+    return files
+
 if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
     # Remove the argument from the stack so we don't cause problems
     # for distutils
     sys.argv.pop(1)
 
     import os
-    import glob
     import shutil
     import subprocess
     from distutils.core import setup
@@ -60,9 +65,9 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
                     print 'ERROR - msgfmt failed with return code %i.' % retcode
 
     DATA_FILES = [
-        ('activity', glob.glob('activity/*')),
-        ('icons', glob.glob('icons/*')),
-        ('images', glob.glob('images/*')),
+        ('activity', get_files('activity/')),
+        ('icons', get_files('icons/')),
+        ('images', get_files('images/')),
         ('/usr/share/applications', ['turtleblocks.desktop'])
     ]
 

commit 0887c2c5bfcf1eb251af4b350916c1aa0f114834

Fix bug introduced in 70e9d98 for samples picker.

TurtleArtActivity.py
@@ -1744,11 +1744,13 @@ class TurtleArtActivity(activity.Activity):
             store.append([pixbuf, filepath])
 
     def _scan_for_samples(self):
-        path = os.path.join(self._share_path, "samples/thumbnails")
+        path = os.path.join(activity.get_bundle_path(),
+                                            'samples', 'thumbnails')
         samples = []
         for name in os.listdir(path):
             if name.endswith(".png"):
-                samples.append(os.path.join(self._share_path, name))
+                samples.append(os.path.join(activity.get_bundle_path(),
+                                            'samples', 'thumbnails', name))
         samples.sort()
         return samples
 