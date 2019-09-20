sugarlabs/turtleart-activity

commit e3f8fa9d69041f4912fce5f624397c82018a8ffb

GTK3 and GST1.0 port

TurtleArt/tacollaboration.py
@@ -19,27 +19,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from dbus.service import signal
-from dbus.gobject_service import ExportedGObject
-import telepathy
 import os
-import gtk
-import tempfile
+import base64
+import telepathy
 
 from gettext import gettext as _
 
-from TurtleArt.tautils import (data_to_string, data_from_string, get_path,
-                               base64_to_image, debug_output, error_output)
+from dbus.service import signal
+from dbus.gobject_service import ExportedGObject
+
+from gi.repository import GdkPixbuf
+
+from TurtleArt.tautils import data_to_string, data_from_string, get_path, \
+                              base64_to_image, debug_output, error_output
 from TurtleArt.taconstants import DEFAULT_TURTLE_COLORS
 
+from sugar3 import profile
+from sugar3.presence import presenceservice
+
 try:
-    from sugar import profile
-    from sugar.presence import presenceservice
-    from sugar.presence.tubeconn import TubeConnection
-except:
-    profile = None
-    from collaboration import presenceservice
-    from collaboration.tubeconn import TubeConnection
+    from sugar3.presence.wrapper import CollabWrapper
+except ImportError:
+    from textchannelwrapper import CollabWrapper
+
 
 SERVICE = 'org.laptop.TurtleArtActivity'
 IFACE = SERVICE
@@ -172,15 +174,9 @@ class Collaboration():
                 self.tubes_chan[telepathy.CHANNEL_TYPE_TUBES]\
                     .AcceptDBusTube(id)
 
-            tube_conn = TubeConnection(
-                self.conn,
-                self.tubes_chan[telepathy.CHANNEL_TYPE_TUBES],
-                id,
-                group_iface=self.text_chan[telepathy.CHANNEL_INTERFACE_GROUP])
-
-            # We'll use a chat tube to send serialized stacks back and forth.
-            self.chattube = ChatTube(tube_conn, self.initiating,
-                                     self.event_received_cb)
+            self.collab = CollabWrapper(self)
+            self.collab.message.connect(self.event_received_cb)
+            self.collab.setup()
 
             # Now that we have the tube, we can ask for the turtle dictionary.
             if self.waiting_for_turtles:  # A joiner must wait for turtles.
@@ -188,11 +184,11 @@ class Collaboration():
                              self._tw.running_sugar)
                 # We need to send our own nick, colors, and turtle position
                 colors = self._get_colors()
-                event = 't|' + data_to_string([self._get_nick(), colors])
+                event = data_to_string([self._get_nick(), colors])
                 debug_output(event, self._tw.running_sugar)
-                self.send_event(event)
+                self.send_event("t", {"payload": event})
 
-    def event_received_cb(self, event_message):
+    def event_received_cb(self, colab, buddy, msg):
         """
         Events are sent as a tuple, nick|cmd, where nick is a turle name
         and cmd is a turtle event. Everyone gets the turtle dictionary from
@@ -205,22 +201,19 @@ class Collaboration():
        # Save active Turtle
         save_active_turtle = self._tw.turtles.get_active_turtle()
 
-        try:
-            command, payload = event_message.split('|', 2)
-        except ValueError:
-            debug_output('Could not split event message [%s]' % event_message,
-                         self._tw.running_sugar)
-
+        command = msg.get("msg")
+        payload = msg.get("payload")
         self._processing_methods[command](payload)
 
         # Restore active Turtle
         self._tw.turtles.set_turtle(
             self._tw.turtles.get_turtle_key(save_active_turtle))
 
-    def send_event(self, entry):
+    def send_event(self, message, payload):
         """ Send event through the tube. """
         if hasattr(self, 'chattube') and self.chattube is not None:
-            self.chattube.SendText(entry)
+            payload["msg"] = message
+            self.chattube.post(payload)
 
     def _turtle_request(self, payload):
         ''' incoming turtle from a joiner '''
@@ -247,7 +240,7 @@ class Collaboration():
                 self._tw.remote_turtle_dictionary[self._tw.nick] = \
                     self._get_colors()
             event_payload = data_to_string(self._tw.remote_turtle_dictionary)
-            self.send_event('T|' + event_payload)
+            self.send_event("T", {"payload": event_payload})
             self.send_my_xy()  # And the sender should report her xy position.
 
     def _receive_turtle_dict(self, payload):
@@ -283,21 +276,20 @@ class Collaboration():
         used to sync positions after turtle drag. '''
         self._tw.turtles.set_turtle(self._get_nick())
         if self._tw.turtles.get_active_turtle().get_pen_state():
-            self.send_event('p|%s' % (data_to_string([self._get_nick(),
-                                                      False])))
+            self.send_event("p", {"payload": data_to_string([self._get_nick(),
+                                                      False])})
             put_pen_back_down = True
         else:
             put_pen_back_down = False
-        self.send_event('x|%s' % (data_to_string(
-            [self._get_nick(),
+        self.send_event("x", {"payload": data_to_string([self._get_nick(),
              [int(self._tw.turtles.get_active_turtle().get_xy()[0]),
-              int(self._tw.turtles.get_active_turtle().get_xy()[1])]])))
+              int(self._tw.turtles.get_active_turtle().get_xy()[1])]])})
         if put_pen_back_down:
-            self.send_event('p|%s' % (data_to_string([self._get_nick(),
-                                                      True])))
-        self.send_event('r|%s' % (data_to_string(
+            self.send_event("p", {"payload": data_to_string([self._get_nick(),
+                                                      True])})
+        self.send_event("r", {"payload": data_to_string(
             [self._get_nick(),
-             int(self._tw.turtles.get_active_turtle().get_heading())])))
+             int(self._tw.turtles.get_active_turtle().get_heading())])})
 
     def _reskin_turtle(self, payload):
         if len(payload) > 0:
@@ -306,9 +298,9 @@ class Collaboration():
                 if self._tw.running_sugar:
                     tmp_path = get_path(self._tw.activity, 'instance')
                 else:
-                    tmp_path = tempfile.gettempdir()
+                    tmp_path = '/tmp'
                 file_name = base64_to_image(data, tmp_path)
-                pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(file_name,
+                pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(file_name,
                                                               width, height)
                 self._tw.turtles.set_turtle(nick)
                 self._tw.turtles.get_active_turtle().set_shapes([pixbuf])
@@ -321,9 +313,9 @@ class Collaboration():
                 if self._tw.running_sugar:
                     tmp_path = get_path(self._tw.activity, 'instance')
                 else:
-                    tmp_path = tempfile.gettempdir()
+                    tmp_path = '/tmp'
                 file_name = base64_to_image(data, tmp_path)
-                pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(file_name,
+                pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(file_name,
                                                               width, height)
                 pos = self._tw.turtles.turtle_to_screen_coordinates((x, y))
                 self._tw.turtles.get_active_turtle().draw_pixbuf(

TurtleArt/sprites.py
@@ -75,13 +75,15 @@ def svg_str_to_pixbuf(svg_string):
     return pixbuf
 
 '''
-
-import pygtk
-pygtk.require('2.0')
-import gtk
-import pango
-import pangocairo
 import cairo
+import gi
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GdkPixbuf
+from gi.repository import Pango
+from gi.repository import PangoCairo
 
 
 class Sprites:
@@ -171,7 +173,11 @@ class Sprite:
         ''' Initialize an individual sprite '''
         self._sprites = sprites
         self.save_xy = (x, y)  # remember initial (x, y) position
-        self.rect = gtk.gdk.Rectangle(int(x), int(y), 0, 0)
+        self.rect = Gdk.Rectangle()
+        self.rect.x = int(x)
+        self.rect.y = int(y)
+        self.rect.width = image.get_height()
+        self.rect.height = image.get_width()
         self._scale = [12]
         self._rescale = [True]
         self._horiz_align = ['center']
@@ -200,7 +206,7 @@ class Sprite:
             self._dy.append(0)
         self._dx[i] = dx
         self._dy[i] = dy
-        if isinstance(image, gtk.gdk.Pixbuf) or \
+        if isinstance(image, GdkPixbuf.Pixbuf) or \
            isinstance(image, cairo.ImageSurface):
             w = image.get_width()
             h = image.get_height()
@@ -220,8 +226,7 @@ class Sprite:
             surface = cairo.ImageSurface(
                 cairo.FORMAT_ARGB32, self.rect.width, self.rect.height)
             context = cairo.Context(surface)
-            context = gtk.gdk.CairoContext(context)
-            context.set_source_pixbuf(image, 0, 0)
+            Gdk.cairo_set_source_pixbuf(context, image, 0, 0)
             context.rectangle(0, 0, self.rect.width, self.rect.height)
             context.fill()
             self.cached_surfaces[i] = surface
@@ -302,7 +307,7 @@ class Sprite:
 
     def set_font(self, font):
         ''' Set the font for a label '''
-        self._fd = pango.FontDescription(font)
+        self._fd = Pango.FontDescription(font)
 
     def set_label_color(self, rgb):
         ''' Set the font color for a label '''
@@ -387,33 +392,27 @@ class Sprite:
 
     def draw_label(self, cr):
         ''' Draw the label based on its attributes '''
-        # Create a pangocairo context
-        cr = pangocairo.CairoContext(cr)
+
         my_width = self.rect.width - self._margins[0] - self._margins[2]
         if my_width < 0:
             my_width = 0
         my_height = self.rect.height - self._margins[1] - self._margins[3]
         for i in range(len(self.labels)):
-            if hasattr(self, 'pl'):  # Reuse the pango layout
-                pl = self.pl
-            else:
-                self.pl = cr.create_layout()
-                pl = self.pl
-            pl.set_text(str(self.labels[i]))
-            self._fd.set_size(int(self._scale[i] * pango.SCALE))
+            pl = PangoCairo.create_layout(cr)
+            pl.set_text(str(self.labels[i]), len(str(self.labels[i])))
+            self._fd.set_size(int(self._scale[i] * Pango.SCALE))
             pl.set_font_description(self._fd)
-            w = pl.get_size()[0] / pango.SCALE
+            w = pl.get_size()[0] / Pango.SCALE
             if w > my_width:
                 if self._rescale[i]:
-                    self._fd.set_size(
-                        int(self._scale[i] * pango.SCALE * my_width / w))
+                    self._fd.set_size(int(self._scale[i] * Pango.SCALE * my_width / w))
                     pl.set_font_description(self._fd)
-                    w = pl.get_size()[0] / pango.SCALE
+                    w = pl.get_size()[0] / Pango.SCALE
                 else:
-                    pl.set_width(int(my_width * pango.SCALE))
-                    pl.set_ellipsize(pango.ELLIPSIZE_MIDDLE)
-                    pl.set_text(str(self.labels[i]))
-                    w = pl.get_size()[0] / pango.SCALE
+                    pl.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
+                    self._fd.set_size(int(self._scale[i] * Pango.SCALE))
+                    pl.set_font_description(self._fd) 
+                    w = pl.get_size()[0] / Pango.SCALE
             if self._x_pos[i] is not None:
                 x = int(self.rect.x + self._x_pos[i])
             elif self._horiz_align[i] == 'center':
@@ -422,7 +421,7 @@ class Sprite:
                 x = int(self.rect.x + self._margins[0])
             else:  # right
                 x = int(self.rect.x + self.rect.width - w - self._margins[2])
-            h = pl.get_size()[1] / pango.SCALE
+            h = pl.get_size()[1] / Pango.SCALE
             if self._y_pos[i] is not None:
                 y = int(self.rect.y + self._y_pos[i])
             elif self._vert_align[i] == 'middle':
@@ -435,21 +434,22 @@ class Sprite:
             cr.save()
             cr.translate(x, y)
             cr.set_source_rgb(self._color[0], self._color[1], self._color[2])
-            cr.update_layout(pl)
-            cr.show_layout(pl)
+            PangoCairo.update_layout(cr, pl)
+            PangoCairo.show_layout(cr, pl)
+     
             cr.restore()
 
     def label_width(self):
         ''' Calculate the width of a label '''
-        cr = pangocairo.CairoContext(self._sprites.cr)
+        cr = self._sprites.cr
         if cr is not None:
             max = 0
             for i in range(len(self.labels)):
-                pl = cr.create_layout()
-                pl.set_text(self.labels[i])
-                self._fd.set_size(int(self._scale[i] * pango.SCALE))
+                pl = PangoCairo.create_layout(cr)
+                pl.set_text(self.labels[i], len(self.labels[i]))
+                self._fd.set_size(int(self._scale[i] * Pango.SCALE))
                 pl.set_font_description(self._fd)
-                w = pl.get_size()[0] / pango.SCALE
+                w = pl.get_size()[0] / Pango.SCALE
                 if w > max:
                     max = w
             return max

TurtleArt/tabasics.py
@@ -100,17 +100,17 @@ the others. (This is the default icon size for Sugar toolbars.)
 from time import time
 from gettext import gettext as _
 
-from .tapalette import (make_palette, define_logo_function)
-from .talogo import primitive_dictionary
-from .taconstants import (
+from tapalette import (make_palette, define_logo_function)
+from talogo import primitive_dictionary
+from taconstants import (
     Color,
     CONSTANTS,
     DEFAULT_BACKGROUND_COLOR_SHADE_GRAY)
-from .taprimitive import (ArgSlot, ConstantArg, or_, Primitive)
-from .tatype import (TYPE_BOOL, TYPE_BOX, TYPE_CHAR, TYPE_COLOR, TYPE_FLOAT,
+from taprimitive import (ArgSlot, ConstantArg, or_, Primitive)
+from tatype import (TYPE_BOOL, TYPE_BOX, TYPE_CHAR, TYPE_COLOR, TYPE_FLOAT,
                      TYPE_INT, TYPE_NUMBER, TYPE_NUMERIC_STRING, TYPE_OBJECT,
                      TYPE_STRING, TYPE_VECTOR)
-from .taturtle import Turtle
+from taturtle import Turtle
 
 def _millisecond():
     ''' Current time in milliseconds '''

TurtleArt/tablock.py
@@ -19,20 +19,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import gtk
 import cairo
 
-from .taconstants import (EXPANDABLE, EXPANDABLE_ARGS, OLD_NAMES, CONSTANTS,
-                          STANDARD_STROKE_WIDTH, BLOCK_SCALE, BOX_COLORS,
-                          GRADIENT_COLOR, EXPANDABLE_FLOW, Color,
-                          MEDIA_BLOCK2TYPE, BLOCKS_WITH_SKIN)
-from .tapalette import (palette_blocks, block_colors, expandable_blocks,
-                        content_blocks, block_names, block_primitives,
-                        block_styles, special_block_colors)
-from .tasprite_factory import (SVG, svg_str_to_pixbuf)
-from . import sprites
+from gi.repository import Gdk
+from gi.repository import GdkPixbuf
 
-from .tautils import (debug_output, error_output)
+from taconstants import EXPANDABLE, EXPANDABLE_ARGS, OLD_NAMES, CONSTANTS, \
+                        STANDARD_STROKE_WIDTH, BLOCK_SCALE, BOX_COLORS, \
+                        GRADIENT_COLOR, EXPANDABLE_FLOW, Color, \
+                        MEDIA_BLOCK2TYPE, BLOCKS_WITH_SKIN
+
+from tapalette import palette_blocks, block_colors, expandable_blocks, \
+                      content_blocks, block_names, block_primitives, \
+                      block_styles, special_block_colors
+
+from tasprite_factory import SVG, svg_str_to_pixbuf
+import sprites
+
+from tautils import debug_output, error_output
 
 
 media_blocks_dictionary = {}  # new media blocks get added here
@@ -408,7 +412,7 @@ class Block:
         if self.spr is None:
             return
         if self._image is not None:
-            tmp = self._image.scale_simple(w, h, gtk.gdk.INTERP_NEAREST)
+            tmp = self._image.scale_simple(w, h, GdkPixbuf.InterpType.NEAREST)
             self.spr.set_image(tmp, 1, x, y)
 
     def rescale(self, scale):
@@ -1360,8 +1364,7 @@ def _pixbuf_to_cairo_surface(image, width, height):
     surface = cairo.ImageSurface(
         cairo.FORMAT_ARGB32, int(width), int(height))
     context = cairo.Context(surface)
-    context = gtk.gdk.CairoContext(context)
-    context.set_source_pixbuf(image, 0, 0)
+    Gdk.cairo_set_source_pixbuf(context, image, 0, 0)
     context.rectangle(0, 0, int(width), int(height))
     context.fill()
     return surface

TurtleArt/tacanvas.py
@@ -20,15 +20,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import gtk
-from math import pi
-import os
-import pango
 import cairo
-import pangocairo
+import os
 
-from .tautils import get_path
-from .taconstants import (Color, TMP_SVG_PATH, DEFAULT_PEN_COLOR,
+from math import pi
+
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import Pango
+from gi.repository import PangoCairo
+from gi.repository import GObject
+from tautils import get_path
+from taconstants import (Color, TMP_SVG_PATH, DEFAULT_PEN_COLOR, 
                           DEFAULT_BACKGROUND_COLOR, DEFAULT_FONT)
 
 
@@ -117,9 +120,6 @@ class TurtleGraphics:
 
         # Build a cairo.Context from a cairo.XlibSurface
         self.canvas = cairo.Context(self.turtle_window.turtle_canvas)
-        cr = gtk.gdk.CairoContext(self.canvas)
-        cr.set_line_cap(1)  # Set the line cap to be round
-
         self.set_pen_size(5)
 
     def setup_svg_surface(self):
@@ -278,8 +278,7 @@ class TurtleGraphics:
     def draw_surface(self, surface, x, y, w, h):
         ''' Draw a surface '''
 
-        def _draw_surface(cr, surface, x, y, w, h):
-            cc = gtk.gdk.CairoContext(cr)
+        def _draw_surface(cc, surface, x, y, w, h):
             cc.set_source_surface(surface, x, y)
             cc.rectangle(x, y, w, h)
             cc.fill()
@@ -292,16 +291,15 @@ class TurtleGraphics:
     def draw_pixbuf(self, pixbuf, a, b, x, y, w, h, heading):
         ''' Draw a pixbuf '''
 
-        def _draw_pixbuf(cr, pixbuf, a, b, x, y, w, h, heading):
+        def _draw_pixbuf(cc, pixbuf, a, b, x, y, w, h, heading):
             # Build a gtk.gdk.CairoContext from a cairo.Context to access
             # the set_source_pixbuf attribute.
-            cc = gtk.gdk.CairoContext(cr)
             cc.save()
             # center the rotation on the center of the image
             cc.translate(x + w / 2., y + h / 2.)
             cc.rotate(heading * DEGTOR)
             cc.translate(-x - w / 2., -y - h / 2.)
-            cc.set_source_pixbuf(pixbuf, x, y)
+            Gdk.cairo_set_source_pixbuf(cc, pixbuf, x, y)
             cc.rectangle(x, y, w, h)
             cc.fill()
             cc.restore()
@@ -318,33 +316,35 @@ class TurtleGraphics:
     def draw_text(self, label, x, y, size, width, heading, scale):
         ''' Draw text '''
 
-        def _draw_text(cr, label, x, y, size, width, scale, heading, rgb,
+        def _draw_text(cc, label, x, y, size, width, scale, heading, rgb,
                        wrap=False):
             import textwrap
 
-            final_scale = int(size * scale) * pango.SCALE
+            final_scale = int(size * scale) * Pango.SCALE
             label = str(label)
             if wrap:
                 label = '\n'.join(textwrap.wrap(label, int(width / scale)))
 
-            cc = pangocairo.CairoContext(cr)
-            pl = cc.create_layout()
-            fd = pango.FontDescription(self._font)
+            pl = PangoCairo.create_layout(cc)
+            fd = Pango.FontDescription(self._font)
             fd.set_size(final_scale)
             pl.set_font_description(fd)
             if isinstance(label, (str, unicode)):
-                pl.set_text(label.replace('\0', ' '))
+                text = label.replace('\0', ' ')
             elif isinstance(label, (float, int)):
-                pl.set_text(str(label))
+                text = str(label)
             else:
-                pl.set_text(str(label))
-            pl.set_width(int(width) * pango.SCALE)
+                text = label
+            
+
+            pl.set_text(str(label), len(str(label)))
+            pl.set_width(int(width) * Pango.SCALE)
             cc.save()
             cc.translate(x, y)
             cc.rotate(heading * DEGTOR)
-            cr.set_source_rgb(rgb[0] / 255., rgb[1] / 255., rgb[2] / 255.)
-            cc.update_layout(pl)
-            cc.show_layout(pl)
+            cc.set_source_rgb(rgb[0] / 255., rgb[1] / 255., rgb[2] / 255.)
+            PangoCairo.update_layout(cc, pl)
+            PangoCairo.show_layout(cc, pl)
             cc.restore()
 
         width *= scale

TurtleArt/taexportlogo.py
@@ -18,12 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-try:
-    from sugar.datastore import datastore
-    HAS_DATASTORE = True
-except:
-    HAS_DATASTORE = False
 
+from sugar3.datastore import datastore
 from TurtleArt.tapalette import (logo_commands, logo_functions)
 from TurtleArt.taconstants import (TITLEXY, CONSTANTS)
 

TurtleArt/taexportpython.py
@@ -30,11 +30,11 @@ import util.codegen as codegen
 
 # from ast_pprint import * # only used for debugging, safe to comment out
 
-from .talogo import LogoCode
-from .taprimitive import (ast_yield_true, Primitive, PyExportError,
+from talogo import LogoCode
+from taprimitive import (ast_yield_true, Primitive, PyExportError,
                           value_to_ast)
-from .tautils import (find_group, find_top_block, get_stack_name)
-from .tawindow import plugins_in_use
+from tautils import (find_group, find_top_block, get_stack_name)
+from tawindow import plugins_in_use
 
 
 _SETUP_CODE_START = """\

commit 9d64c67b342e86416d24e0865e641182926e5abe

flake8 - F821 undefined name 'event_message'
Regression introduced in e3f8fa9 during the port to latest
CollabWrapper.

This signals that collaboration has not been tested since the port.

TurtleArt/tacollaboration.py
@@ -195,10 +195,8 @@ class Collaboration():
         the sharer and watches for 't' events, which indicate that a new
         turtle has joined.
         """
-        if len(event_message) == 0:
-            return
 
-       # Save active Turtle
+        # Save active Turtle
         save_active_turtle = self._tw.turtles.get_active_turtle()
 
         command = msg.get("msg")