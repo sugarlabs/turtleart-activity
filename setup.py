sugarlabs/turtleart-activity

commit 1000384f975b09ba4dd51034c596164aac46c629

Replace deprecated distutils with setuptools
Disutils is deprecated, https://peps.python.org/pep-0632/#migration-advice
this replaces it with setuptools

Signed-off-by: Chihurumnaya Ibiam <ibiam@sugarlabs.org>

setup.py
@@ -100,8 +100,8 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
     import os
     import shutil
     import subprocess
-    from distutils.core import setup
-    from distutils.command.install import install
+    from setuptools import setup
+    from setuptools.command.install import install
 
     class post_install(install):
         def run(self):
@@ -171,7 +171,7 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
           version='0.9.4',
           packages=['TurtleArt', 'TurtleArt.util'],
           scripts=['turtleblocks'],
-          data_files=DATA_FILES,
+          package_data=DATA_FILES,
           cmdclass={"install": post_install}
           )
 else:

commit 9b2e64c26924e1fa7c8e20e40af5913a78f34442

Fix the build error from the wrong option to setuptools
The turtleart-activity builds failed with the error:

error in Turtle Art setup command: 'package_data' must be a dictionary
mapping package names to lists of string wildcard patterns
Error: module turtleblocks: Child process exited with code 1

According to Python 3.9's Writing the Setup Script document - Installing
Additional Files [1], setup()'s option "package_data" should be replaced
as "data_files" for setuptools.

[1] https://docs.python.org/3.9/distutils/setupscript.html#installing-additional-files

Fixes: 1000384f975b ("Replace deprecated distutils with setuptools")

setup.py
@@ -171,7 +171,7 @@ if len(sys.argv) > 1 and '--no-sugar' == sys.argv[1]:
           version='0.9.4',
           packages=['TurtleArt', 'TurtleArt.util'],
           scripts=['turtleblocks'],
-          package_data=DATA_FILES,
+          data_files=DATA_FILES,
           cmdclass={"install": post_install}
           )
 else: